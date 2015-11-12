be2ce1be4902c9003774eba3ac55205ef33a2a52
so-seanf <so-seanf@saintoswald.com>
2015-11-12 21:27:14 +0000

    - Added StringExtensions and relevant tests to add Extension Methods to string to provide useful, reusable functionality - Added IsNullOrEmpty() string Extension Method to determine if the specified string is null or empty - Added License.txt containing the license information

---
+++ SaintOswald.Libraries.Extensions/SaintOswald.Libraries.Extensions.Tests/StringExtensions/StringExtensionsTests.cs
@@ -0,0 +1,35 @@
+﻿/***********************************************************************************
+ *
+ * Saint Oswald: Libraries - Extensions Tests
+ *
+ * Copyright (c) 2015, Saint Oswald
+ *
+ ***********************************************************************************
+ *
+ * LICENSED UNDER THE MIT LICENSE
+ * SEE LICENSE.TXT IN THE PROJECT ROOT FOR LICENSE INFORMATION
+ *
+ ***********************************************************************************/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+using SaintOswald.Libraries.Extensions.StringExtensions;
+
+namespace SaintOswald.Libraries.Extensions.Tests.StringExtensions
+{
+    [TestClass]
+    public class StringExtensionsTests
+    {
+        #region IsNullOrEmpty
+        [TestMethod]
+        public void IsNullOrEmpty()
+        {
+            string nullString = null;
+            IsTrue(nullString.IsNullOrEmpty());
+            IsTrue("".IsNullOrEmpty());
+
+            IsFalse("Test of IsNullOrEmpty".IsNullOrEmpty());
+        }
+        #endregion
+    }
+}
---
+++ SaintOswald.Libraries.Extensions/SaintOswald.Libraries.Extensions/StringExtensions/StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿/***********************************************************************************
+ *
+ * Saint Oswald: Libraries - Extensions
+ *
+ * Copyright (c) 2015, Saint Oswald
+ *
+ ***********************************************************************************
+ *
+ * LICENSED UNDER THE MIT LICENSE
+ * SEE LICENSE.TXT IN THE PROJECT ROOT FOR LICENSE INFORMATION
+ *
+ ***********************************************************************************/
+
+namespace SaintOswald.Libraries.Extensions.StringExtensions
+{
+    /// <summary>
+    /// Adds Extension Methods to string to provide useful, reusable functionality
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Checks if the specified string is null or empty
+        /// </summary>
+        /// <param name="str">The string to check</param>
+        /// <returns>True if the specified string is null or empty, otherwise returns false</returns>
+        public static bool IsNullOrEmpty(this string str) => string.IsNullOrEmpty(str);
+    }
+}
