4b389a504867ca95e02d2f35256153277036e50b
so-seanf <so-seanf@saintoswald.com>
2015-11-12 20:14:25 +0000

    Initial commit of SaintOswald.Libraries.Extensions Solution structure

---
+++ SaintOswald.Libraries.Extensions/SaintOswald.Libraries.Extensions/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SaintOswald: Libraries - Extensions")]
+[assembly: AssemblyDescription("A collection of useful, reusable Extension Methods")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Saint Oswald")]
+[assembly: AssemblyProduct("SaintOswald.Libraries.Extensions")]
+[assembly: AssemblyCopyright("Copyright © Saint Oswald 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7c1ab548-a755-4b5d-89de-9db09ee0064a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
