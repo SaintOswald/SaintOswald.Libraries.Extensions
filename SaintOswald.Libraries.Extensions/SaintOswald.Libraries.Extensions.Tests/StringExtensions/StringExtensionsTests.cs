b10f17446776d1c932e9ac628964b9b029a86e7d
so-seanf <so-seanf@saintoswald.com>
2015-11-12 22:26:13 +0000

    - Added Truncate() string Extension Method to truncate a specified string, trimming trailing punctuation and white space and adding the suffix "..." - Modified StringExtensionsTests to simplify variable names

--- SaintOswald.Libraries.Extensions/SaintOswald.Libraries.Extensions.Tests/StringExtensions/StringExtensionsTests.cs
+++ SaintOswald.Libraries.Extensions/SaintOswald.Libraries.Extensions.Tests/StringExtensions/StringExtensionsTests.cs
@@ -11,6 +11,7 @@
  *
  ***********************************************************************************/
 
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 using SaintOswald.Libraries.Extensions.StringExtensions;
@@ -24,8 +25,8 @@ namespace SaintOswald.Libraries.Extensions.Tests.StringExtensions
         [TestMethod]
         public void IsNullOrEmpty()
         {
-            string nullString = null;
-            IsTrue(nullString.IsNullOrEmpty());
+            string s = null;
+            IsTrue(s.IsNullOrEmpty());
             IsTrue("".IsNullOrEmpty());
 
             IsFalse("Test of IsNullOrEmpty".IsNullOrEmpty());
@@ -36,13 +37,71 @@ namespace SaintOswald.Libraries.Extensions.Tests.StringExtensions
         [TestMethod]
         public void IsNullOrWhiteSpace()
         {
-            string nullString = null;
-            IsTrue(nullString.IsNullOrWhiteSpace());
+            string s = null;
+            IsTrue(s.IsNullOrWhiteSpace());
             IsTrue("".IsNullOrWhiteSpace());
             IsTrue(" ".IsNullOrWhiteSpace());
             IsTrue(System.Environment.NewLine.IsNullOrWhiteSpace());
 
             IsFalse("Test of IsNullOrWhitespace".IsNullOrWhiteSpace());
+        }
+        #endregion
+
+        #region Truncate
+        [TestMethod]
+        public void TestTruncate()
+        {
+            AreEqual("Test of trun...", "Test of truncation".Truncate(15));
+        }
+
+        [TestMethod]
+        public void TestTruncateStringShorterThanMaximumLengthReturnsOriginal()
+        {
+            AreEqual("Test of truncation", "Test of truncation".Truncate(100));
+        }
+
+        [TestMethod]
+        public void TestTruncateStringEqualToMaximumLengthReturnsOriginal()
+        {
+            AreEqual("Test of truncation", "Test of truncation".Truncate(18));
+        }
+
+        [TestMethod]
+        public void TestTruncateSuffixDoesNotExceedMaximumLength()
+        {
+            AreEqual("Test of trunca...", "Test of truncation".Truncate(17));
+        }
+
+        [TestMethod]
+        public void TestTruncateStripsTrailingPunctuation()
+        {
+            AreEqual("Test of...", "Test of??????????truncation".Truncate(12));
+        }
+
+        [TestMethod]
+        public void TestTruncateStripsTrailingWhiteSpace()
+        {
+            AreEqual("Test of...", "Test of          truncation".Truncate(12));
+        }
+
+        [TestMethod]
+        public void TestTruncateNullReturnsNull()
+        {
+            string s = null;
+            IsNull(s.Truncate(15));
+        }
+
+        [TestMethod]
+        public void TestTruncateStringEmptyReturnsEmpty()
+        {
+            AreEqual("", "".Truncate(15));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestTruncateMaximumLengthLessThanThreeThrowsException()
+        {
+            "Test of truncation".Truncate(2);
         }
         #endregion
     }
--- SaintOswald.Libraries.Extensions/SaintOswald.Libraries.Extensions/StringExtensions/StringExtensions.cs
+++ SaintOswald.Libraries.Extensions/SaintOswald.Libraries.Extensions/StringExtensions/StringExtensions.cs
@@ -11,6 +11,9 @@
  *
  ***********************************************************************************/
 
+using System;
+using System.Text.RegularExpressions;
+
 namespace SaintOswald.Libraries.Extensions.StringExtensions
 {
     /// <summary>
@@ -33,5 +36,32 @@ namespace SaintOswald.Libraries.Extensions.StringExtensions
         /// <returns>True if the specified string is null or whitespace, otherwise returns false</returns>
         /// <seealso cref="System.String.IsNullOrWhiteSpace(string)"/>
         public static bool IsNullOrWhiteSpace(this string str) => string.IsNullOrWhiteSpace(str);
+
+        /// <summary>
+        /// Truncates the specified string to the given maximum length.  The string will be returned
+        /// as-is if it's length is shorter than or equal to the maximum length.  A truncated string
+        /// will be suffixed with "..." and the truncated string plus the suffix will be no longer
+        /// than the maximum length.  Trailing punctuation and white space will be removed from
+        /// truncated strings
+        /// </summary>
+        /// <param name="str">The string to truncate</param>
+        /// <param name="maximumLength">
+        /// The maximum length of the truncated string - must be at least 3
+        /// </param>
+        /// <returns>The truncated string</returns>
+        /// <exception cref="System.ArgumentException">
+        /// Thrown when the specified maximum length is less than 3
+        /// </exception>
+        public static string Truncate(this string str, int maximumLength)
+        {
+            if (maximumLength < 3)
+            {
+                throw new ArgumentException("Truncate maximum length cannot be less than 3", nameof(maximumLength));
+            }
+
+            if (str.IsNullOrEmpty() || str.Length <= maximumLength) { return str; }
+
+            return Regex.Replace(str.Substring(0, maximumLength - 3), @"[\W_]+$", "") + "...";
+        }
     }
 }
