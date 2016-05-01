1f246323d32740e7a9bcb4333ac34a80e2951fca
so-seanf <so-seanf@saintoswald.com>
2016-05-01 20:39:03 +0100

    -  Initial import of LongExtensions to contain various useful, reusable Extension Methods for long - Added ToFileSize() long Extension Method to convert the specified byte value to human readable file size format

---
+++ SaintOswald.Libraries.Extensions/SaintOswald.Libraries.Extensions.Tests/LongExtensions/LongExtensionsTests.cs
@@ -0,0 +1,96 @@
+﻿/***********************************************************************************
+ *
+ * Saint Oswald: Libraries - Extensions Tests
+ *
+ * Copyright (c) 2015 - 2016, Saint Oswald
+ *
+ ***********************************************************************************
+ *
+ * LICENSED UNDER THE MIT LICENSE
+ * SEE LICENSE FILE IN THE PROJECT ROOT FOR LICENSE INFORMATION
+ *
+ ***********************************************************************************/
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+using SaintOswald.Libraries.Extensions.LongExtensions;
+
+namespace SaintOswald.Libraries.Extensions.Tests.LongExtensions
+{
+    [TestClass]
+    public class LongExtensionsTests
+    {
+        #region ToFileSize
+        [TestMethod]
+        public void TestToFileSize()
+        {
+            long bytes = 1099511627776;
+            AreEqual("1 TB", bytes.ToFileSize());
+
+            bytes = 1073741824;
+            AreEqual("1 GB", bytes.ToFileSize());
+
+            bytes = 1048576;
+            AreEqual("1 MB", bytes.ToFileSize());
+
+            bytes = 1024;
+            AreEqual("1 KB", bytes.ToFileSize());
+
+            bytes = 1;
+            AreEqual("1 byte", bytes.ToFileSize());
+
+            bytes = 0;
+            AreEqual("0 bytes", bytes.ToFileSize());
+        }
+
+        [TestMethod]
+        public void TestToFileSizeRoundsResult()
+        {
+            long bytes = 1715238139330;
+            AreEqual("1.56 TB", bytes.ToFileSize());
+
+            bytes = 1438814044;
+            AreEqual("1.34 GB", bytes.ToFileSize());
+
+            bytes = 1960837;
+            AreEqual("1.87 MB", bytes.ToFileSize());
+
+            bytes = 1351;
+            AreEqual("1.32 KB", bytes.ToFileSize());
+        }
+
+        [TestMethod]
+        public void TestToFileSizeSpecifyDecimals()
+        {
+            long bytes = 1723914605487;
+            AreEqual("1.5678912 TB", bytes.ToFileSize(7));
+
+            bytes = 1444000217;
+            AreEqual("1.34483 GB", bytes.ToFileSize(5));
+
+            bytes = 1967128;
+            AreEqual("1.876 MB", bytes.ToFileSize(3));
+
+            bytes = 1351;
+            AreEqual("1.3 KB", bytes.ToFileSize(1));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestToFileSizeDecimalsLessThanOneThrowsException()
+        {
+            long bytes = 1715238139330;
+            bytes.ToFileSize(0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestToFileSizeDecimalsGreaterThanTwentyEightThrowsException()
+        {
+            long bytes = 1715238139330;
+            bytes.ToFileSize(29);
+        }
+        #endregion
+    }
+}
---
+++ SaintOswald.Libraries.Extensions/SaintOswald.Libraries.Extensions/LongExtensions/LongExtensions.cs
@@ -0,0 +1,63 @@
+﻿/***********************************************************************************
+ *
+ * Saint Oswald: Libraries - Extensions
+ *
+ * Copyright (c) 2015 - 2016, Saint Oswald
+ *
+ ***********************************************************************************
+ *
+ * LICENSED UNDER THE MIT LICENSE
+ * SEE LICENSE FILE IN THE PROJECT ROOT FOR LICENSE INFORMATION
+ *
+ ***********************************************************************************/
+
+using System;
+
+namespace SaintOswald.Libraries.Extensions.LongExtensions
+{
+    /// <summary>
+    /// Adds Extension Methods to long to provide useful, reusable functionality
+    /// </summary>
+    public static class LongExtensions
+    {
+        /// <summary>
+        /// Converts the specified byte value to human readable file size format
+        /// </summary>
+        /// <param name="bytes">The byte value to convert</param>
+        /// <param name="decimals">The number of decimal places in the formatted value</param>
+        /// <returns>
+        /// The specified byte value formatted as a human readable file size string
+        /// </returns>
+        /// <exception cref="System.ArgumentOutOfRangeException">
+        /// Thrown when the specified decimals value is less than 1 or greater than 28
+        /// </exception>
+        public static string ToFileSize(this long bytes, int decimals = 2)
+        {
+            if (decimals < 1 || decimals > 28)
+            {
+                throw new ArgumentOutOfRangeException(nameof(decimals), "Decimals must be at least 1 and no bigger than 28");
+            }
+
+            if (bytes >= 1099511627776)
+            {
+                return Math.Round(((decimal)bytes / 1024 / 1024 / 1024 / 1024), decimals) + " TB";
+            }
+            else if (bytes >= 1073741824)
+            {
+                return Math.Round(((decimal)bytes / 1024 / 1024 / 1024), decimals) + " GB";
+            }
+            else if (bytes >= 1048576)
+            {
+                return Math.Round(((decimal)bytes / 1024 / 1024), decimals) + " MB";
+            }
+            else if (bytes >= 1024)
+            {
+                return Math.Round(((decimal)bytes / 1024), decimals) + " KB";
+            }
+            else
+            {
+                return bytes + (bytes == 1 ? " byte" : " bytes");
+            }
+        }
+    }
+}
