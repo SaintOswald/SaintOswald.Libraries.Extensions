9f52969075bfa94583ce854008a9817bd1ce5a74
so-seanf <so-seanf@saintoswald.com>
2016-05-07 21:03:31 +0100

    -  Initial import of FileInfoExtensions to contain various useful, reusable Extension Methods for FileInfo - Added LengthFormatted() FileInfo Extension Method to return the file length in human readable file size format - Bumped version to 1.2.3

---
+++ SaintOswald.Libraries.Extensions/SaintOswald.Libraries.Extensions.Tests/FileInfoExtensions/FileInfoExtensionsTests.cs
@@ -0,0 +1,37 @@
+﻿/***********************************************************************************
+ *
+ * Saint Oswald: Libraries - Extensions Tests
+ *
+ * Copyright (c) 2015 - 2016, Saint Oswald
+ *
+ ***********************************************************************************
+ *
+ * LICENSED UNDER THE MIT LICENSE
+ * SEE LICENSE FILE IN THE PROJECT ROOT FOR LICENSE INFORMATION
+ *
+ ***********************************************************************************/
+
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+using SaintOswald.Libraries.Extensions.FileInfoExtensions;
+
+namespace SaintOswald.Libraries.Extensions.Tests.FileInfoExtensions
+{
+    [TestClass]
+    public class FileInfoExtensionsTests
+    {
+        #region ToFileSize
+        [TestMethod]
+        public void TestLengthFormatted()
+        {
+            FileInfo file = new FileInfo("FileInfoExtensionsTestFile.txt");
+
+            AreEqual(3327, file.Length);
+            AreEqual("3.25 KB", file.LengthFormatted());
+
+            file = null;
+        }
+        #endregion
+    }
+}
---
+++ SaintOswald.Libraries.Extensions/SaintOswald.Libraries.Extensions/FileInfoExtensions/FileInfoExtensions.cs
@@ -0,0 +1,35 @@
+﻿/***********************************************************************************
+ *
+ * Saint Oswald: Libraries - Extensions
+ *
+ * Copyright (c) 2015 - 2016, Saint Oswald
+ *
+ ***********************************************************************************
+ *
+ * LICENSED UNDER THE MIT LICENSE
+ * SEE LICENSE FILE IN THE PROJECT ROOT FOR LICENSE INFORMATION
+ *
+ ***********************************************************************************/
+
+using System.IO;
+using SaintOswald.Libraries.Extensions.LongExtensions;
+
+namespace SaintOswald.Libraries.Extensions.FileInfoExtensions
+{
+    /// <summary>
+    /// Adds Extension Methods to FileInfo to provide useful, reusable functionality
+    /// </summary>
+    public static class FileInfoExtensions
+    {
+        /// <summary>
+        /// Returns the file length in human readable file size format
+        /// </summary>
+        /// <returns>
+        /// The file length formatted as a human readable file size string
+        /// </returns>
+        public static string LengthFormatted(this FileInfo file)
+        {
+            return file.Length.ToFileSize();
+        }
+    }
+}
